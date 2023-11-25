64fd3747197a870773f414b5ae8546152ae932bc
11111100DK <lndksky@gmail.com>
2023-11-25 14:24:36 +0700

    revert

--- spring-restful-api/src/main/java/com/example/springrestful/model/request/RequestRecruiter/RequestRecruiter.java
+++ spring-restful-api/src/main/java/com/example/springrestful/model/request/RequestRecruiter/RequestRecruiter.java
@@ -1,33 +1,4 @@
 package com.example.springrestful.model.request.RequestRecruiter;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class RequestRecruiter {
-    private int id;
-
-    @NotBlank(message = "Title is required")
-    @Size(max = 100, message = "Title must be less than 100 chars")
-    private String companyName;
-
-    @NotBlank(message = "Address is required")
-    private String companyAddress;
-
-    @NotBlank(message = "Description is required")
-    private String companyDescription;
-    private String fullName;
-    private String linkWebsite;
-    private String linkFacebook;
-    private String companyPhone;
-
-
-    private int accountId;
-    private MultipartFile logo;
-
 }
--- spring-restful-api/src/main/java/com/example/springrestful/model/response/ResponseRecruiter/ResponseRecruiter.java
+++ spring-restful-api/src/main/java/com/example/springrestful/model/response/ResponseRecruiter/ResponseRecruiter.java
@@ -1,28 +1,4 @@
 package com.example.springrestful.model.response.ResponseRecruiter;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-
-
-import java.util.Date;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ResponseRecruiter {
-    private int id;
-    private String fullName;
-    private String companyName;
-    private String linkWebsite;
-    private String linkFacebook;
-    private String companyAddress;
-    private String companyDescription;
-    private String companyPhone;
-    private int accountId;
 }
